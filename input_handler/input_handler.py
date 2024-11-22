1836a2a4332908bc4ba543f9074512aa0da4d6dc
Gagan <gkaur21.rrc.ca>
2024-11-22 14:49:59 -0600

    Updated input_handler file by adding required documentation and comments to make the file easy to read.

--- input_handler/input_handler.py
+++ input_handler/input_handler.py
@@ -1,35 +1,68 @@
-"""REQUIRED MODULE DOCUMENTATION
 """
+Description: This program is reading and validating the input data from a CSV file, and implementing custom data validation rules.
+Name: Gaganpreet Kaur
+Date: 11-22-2024
+"""
+
 
 __author__ = ""
 __version__ = ""
 
+# IMPORTS
 import csv
 import json
 from os import path
 
+# CLASS
 class InputHandler:
-    """REQUIRED: CLASS DOCSTRING
+    """
+    class: InputHandler
+    Purpose: This class is reading and validating the input data from a CSV file, and implementing custom data validation rules.
+    
+    Attributes: 
+        file_path(str): The path of a file.
+    Methods: 
+        __init__(self,file_path)
+        file_path(self) -> str
+        get_file_format(self) -> str
+        read_input_data(self) -> list
+        read_csv_data(self) -> list
+        read_json_data(self) -> list
     """
 
+# METHODS
     def __init__(self, file_path: str):
-        """REQUIRED: METHOD DOCSTRING
         """
+        Initializes the class with the given parameters.
+
+        Parameters:
+        - file_path (str): This is a path to the file.
+        """
+
         self.__file_path = file_path
 
-    @property
+    @property   ## ACCESSOR
     def file_path(self) -> str:
-        """REQUIRED: METHOD DOCSTRING
+        """
+        This is a accessor method which allows controlled access to a private attribute.
+        Return:
+            str
         """
         return self.__file_path
 
     def get_file_format(self) -> str:
-        """REQUIRED: METHOD DOCSTRING
+        """
+        This function is taking the file and extracting from file path.
+        Return:
+            str
         """
         return self.__file_path.split(".")[-1]
 
     def read_input_data(self) -> list:
-        """REQUIRED: METHOD DOCSTRING
+        """
+        This method is reading the file after choosing the format and saving it to a transactions list.
+        Return:
+            list
         """
         transactions = []
         file_format = self.get_file_format()
@@ -41,7 +74,12 @@ class InputHandler:
         return transactions
 
     def read_csv_data(self) -> list:
-        """REQUIRED: METHOD DOCSTRING
+        """
+        This method is opening the csv file and reading it.
+        Return:
+            list
+        Raises:
+            ValueError: "Invalid file extension to perform actions"
         """
         if not path.isfile(self.__file_path):
             raise FileNotFoundError(f"File: {self.__file_path} does not exist.")
@@ -56,11 +94,18 @@ class InputHandler:
         return transactions
             
     def read_json_data(self) -> list:
-        """REQUIRED: METHOD DOCSTRING
+        """
+        This method is opening the json file and reading it.
+        Return 
+            list
+        Raises:
+            ValueError: "Invalid file extension to perform actions"
+
         """
         # Research the json.load function so that you 
         # understand the format of the data once it is
         # placed into input_data
+        
         if not path.isfile(self.__file_path):
             raise FileNotFoundError(f"File: {self.__file_path} does not exist.")
 
